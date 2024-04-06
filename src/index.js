56b454fe245a67127c8a8008d44137ed9a7228f1
Sergei Arzamasov <sergei.arzamasov@gmail.com>
2024-04-07 01:19:35 +0300

    add all basic math operations: +, -, *, /

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ const assert = require('assert');
 
 class Eva {
   eval(exp) {
+    // ------------------------------------------------------------
+    // Self-evaluating expressions: 
+
     if (isNumber(exp)) {
       return exp;
     }
@@ -16,6 +19,18 @@ class Eva {
 
     if (exp[0] === '+') {
       return this.eval(exp[1]) + this.eval(exp[2]);
+    }
+
+    if (exp[0] === '-') {
+      return this.eval(exp[1]) - this.eval(exp[2]);
+    }
+
+    if (exp[0] === '*') {
+      return this.eval(exp[1]) * this.eval(exp[2]);
+    }
+
+    if (exp[0] === '/') {
+      return this.eval(exp[1]) / this.eval(exp[2]);
     }
 
     throw 'Unimplemented';
@@ -43,5 +58,9 @@ assert.strictEqual(eva.eval('"hello"'), 'hello');
 
 assert.strictEqual(eva.eval(['+', 1, 5]), 6);
 assert.strictEqual(eva.eval(['+', ['+', 3, 2], 5]), 10);
+assert.strictEqual(eva.eval(['-', ['+', 3, 2], 5]), 0);
+assert.strictEqual(eva.eval(['*', 3, 5]), 15);
+assert.strictEqual(eva.eval(['/', 16, 4]), 4);
+assert.strictEqual(eva.eval(['/', 12, 4]), 3);
 
 console.log('\x1b[32m', 'All assertions passed!');
