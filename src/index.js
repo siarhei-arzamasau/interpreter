9219f6a937a660608c3c9d1a1b3d42f10778269f
Sergei Arzamasov <sergei.arzamasov@gmail.com>
2024-04-07 01:08:50 +0300

    add basic math addition operation

--- src/index.js
+++ src/index.js
@@ -14,6 +14,10 @@ class Eva {
       return exp.slice(1, -1);
     }
 
+    if (exp[0] === '+') {
+      return this.eval(exp[1]) + this.eval(exp[2]);
+    }
+
     throw 'Unimplemented';
   }
 }
@@ -26,6 +30,8 @@ function isString(exp) {
   return typeof exp === 'string' && exp[0] === '"' && exp.slice(-1) === '"';
 }
 
+
+
 // ------------------------------------------------------------
 // Tests:
 
@@ -34,5 +40,8 @@ const eva = new Eva();
 assert.strictEqual(eva.eval(1), 1);
 assert.strictEqual(eva.eval(42), 42);
 assert.strictEqual(eva.eval('"hello"'), 'hello');
+
+assert.strictEqual(eva.eval(['+', 1, 5]), 6);
+assert.strictEqual(eva.eval(['+', ['+', 3, 2], 5]), 10);
 
 console.log('\x1b[32m', 'All assertions passed!');
