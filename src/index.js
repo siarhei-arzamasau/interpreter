2643c174dc7285afd30f7ea71fd910e5405199ee
Sergei Arzamasov <sergei.arzamasov@gmail.com>
2024-04-27 22:58:19 +0300

    add posibility to predefine global environment

--- src/index.js
+++ src/index.js
@@ -52,7 +52,7 @@ class Eva {
     // Variable declaration:
     if (exp[0] === 'var') {
       const [_, name, value] = exp;
-      return env.define(name, value);
+      return env.define(name, this.eval(value));
     }
 
     // ------------------------------------------------------------
@@ -80,7 +80,14 @@ function isVariableName(exp) {
 // ------------------------------------------------------------
 // Tests:
 
-const eva = new Eva();
+const eva = new Eva(new Environment({
+  null: null,
+
+  true: true,
+  false: false,
+
+  VERSION: '0.1',
+}));
 
 assert.strictEqual(eva.eval(1), 1);
 assert.strictEqual(eva.eval(42), 42);
@@ -99,5 +106,10 @@ assert.strictEqual(eva.eval(['var', 'x', 10]), 10);
 assert.strictEqual(eva.eval('x'), 10);
 assert.strictEqual(eva.eval(['var', 'y', 100]), 100);
 assert.strictEqual(eva.eval('y'), 100);
+assert.strictEqual(eva.eval('VERSION'), '0.1');
+assert.strictEqual(eva.eval(['var', 'isUser', 'true']), true);
+assert.strictEqual(eva.eval('isUser'), true);
+assert.strictEqual(eva.eval(['var', 'z', ['*', 2, 2]]), 4);
+assert.strictEqual(eva.eval('z'), 4);
 
 console.log('\x1b[32m', 'All assertions passed!');
