337e1bce61b6260dea94b08524b2129a2bec43c4
Sergei Arzamasov <sergei.arzamasov@gmail.com>
2024-04-27 22:22:48 +0300

    add tests for math operations

--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ const assert = require('assert');
 class Eva {
   eval(exp) {
     // ------------------------------------------------------------
-    // Self-evaluating expressions: 
+    // Self-evaluating expressions:
 
     if (isNumber(exp)) {
       return exp;
     }
-    
+
     if (isString(exp)) {
       return exp.slice(1, -1);
     }
@@ -33,7 +33,7 @@ class Eva {
       return this.eval(exp[1]) / this.eval(exp[2]);
     }
 
-    throw 'Unimplemented';
+    throw `Unimplemented: ${JSON.stringify(exp)}`;
   }
 }
 
@@ -45,8 +45,6 @@ function isString(exp) {
   return typeof exp === 'string' && exp[0] === '"' && exp.slice(-1) === '"';
 }
 
-
-
 // ------------------------------------------------------------
 // Tests:
 
@@ -56,11 +54,15 @@ assert.strictEqual(eva.eval(1), 1);
 assert.strictEqual(eva.eval(42), 42);
 assert.strictEqual(eva.eval('"hello"'), 'hello');
 
+// Math operations
 assert.strictEqual(eva.eval(['+', 1, 5]), 6);
 assert.strictEqual(eva.eval(['+', ['+', 3, 2], 5]), 10);
 assert.strictEqual(eva.eval(['-', ['+', 3, 2], 5]), 0);
 assert.strictEqual(eva.eval(['*', 3, 5]), 15);
 assert.strictEqual(eva.eval(['/', 16, 4]), 4);
 assert.strictEqual(eva.eval(['/', 12, 4]), 3);
+
+// Set expressions
+assert.strictEqual(eva.eval(['var', 'x', 10]), 10);
 
 console.log('\x1b[32m', 'All assertions passed!');
