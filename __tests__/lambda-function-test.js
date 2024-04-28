07341a4d059ea478a19bf4146e586cee23466663
Sergei Arzamasov <sergei.arzamasov@gmail.com>
2024-04-29 00:00:09 +0300

    add IILE

--- __tests__/lambda-function-test.js
+++ __tests__/lambda-function-test.js
@@ -21,4 +21,23 @@ module.exports = (eva) => {
   `,
     300
   );
+
+  // Immediatly-invoked lambda expression - IILE
+  test(
+    eva,
+    `
+    ((lambda (x) (* x x)) 2)
+  `,
+    4
+  );
+
+  // Save lambda to a variable
+  test(eva,
+  `
+    (begin
+      (var square (lambda (x) (* x x)))
+      (square 2)
+    )
+  `,
+  4)
 };
