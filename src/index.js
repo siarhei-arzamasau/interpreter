640b607059abe9419c55c35375e6909ddb0a8350
Sergei Arzamasov <sergei.arzamasov@gmail.com>
2024-04-29 00:06:02 +0300

    add syntactic sugar for functions

--- src/index.js
+++ src/index.js
@@ -81,17 +81,17 @@ class Eva {
 
     // ------------------------------------------------------------
     // Function declaration: (def square (x) (* x x))
+    //
+    // Syntactic sugar for: (var square (lambda (x) (* x x)))
 
     if (exp[0] === 'def') {
       const [_tag, name, params, body] = exp;
 
-      const fn = {
-        params,
-        body,
-        env, // Closure!
-      };
+      // JIT-transpile to a variable declaration
 
-      return env.define(name, fn);
+      const varExp = ['var', name, ['lambda', params, body]];
+
+      return this.eval(varExp, env);
     }
 
     // ------------------------------------------------------------
@@ -104,7 +104,7 @@ class Eva {
         params,
         body,
         env, // Closure!
-      }
+      };
     }
 
     // ------------------------------------------------------------
