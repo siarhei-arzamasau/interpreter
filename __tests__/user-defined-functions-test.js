12ffa9d58c10412bb6dcb51174405265e5584714
Sergei Arzamasov <sergei.arzamasov@gmail.com>
2024-04-29 23:29:12 +0300

    add tests for recursive calls

--- __tests__/user-defined-functions-test.js
+++ __tests__/user-defined-functions-test.js
@@ -18,7 +18,6 @@ module.exports = (eva) => {
     4
   );
 
-
   // Complex body:
   test(
     eva,
@@ -66,5 +65,24 @@ module.exports = (eva) => {
     )
   `,
     160
+  );
+
+  // Recursive function:
+  test(
+    eva,
+    `
+    (begin
+    
+      (def factorial (x)
+        (if (= x 1)
+        1
+        (* x (factorial (- x 1)))
+        )
+      )
+      
+      (factorial 5)
+    )
+  `,
+    120
   );
 };
